5913bcc639f19687bda0b44bd8ab4ff6001787f7
VAIBHAV JAIN <10sunny1998@gmail.com>
2021-03-16 02:33:37 +0530

    navbar complete

--- controllers/home.js
+++ controllers/home.js
@@ -1,8 +1,21 @@
+/* -------- Models Import -------- */
+const Ques = require('../models/question');
+const Interest = require('../models/interest');
 exports.getHome = async (req, res, next) => {
-  try {    
+  try {
+    const interest = await Interest.find({}, { category: true }).lean();    
+    const questions = await Ques.find(
+      {},
+      { title: true, interests: true }
+    ).lean();
+    for (const q of questions) {
+      q.interests = q.interests.split(',');
+    }    
     res.render('frontend/index', {
       pageTitle: 'Home',
-      path: '/',      
+      path: '/',
+      interest,
+      questions,
     });
   } catch (err) {
     const error = new Error(err);
