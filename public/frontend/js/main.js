2f78be780bc8d763dfd298d27d6e354c36df9d87
VAIBHAV JAIN <10sunny1998@gmail.com>
2021-03-20 12:52:49 +0530

    side menu added

--- public/frontend/js/main.js
+++ public/frontend/js/main.js
@@ -1,61 +1,32 @@
-// /* -----------------------  Menu SearchBar DropDown   ----------------------- */
-// const backdrop = document.querySelector('.backdrop');
-// const menuToggler = document.querySelector('#mobile-nav .mobile-nav__btn');
-// const menu = document.querySelector('#mobile-nav .mobile-nav__menu');
-// const searchBtnMain = document.querySelector('#searchBtn-main');
-// const searchBtnMob = document.querySelector('#searchBtn-mob');
-// const searchBar = document.querySelector('#searchBar');
-// const closeBtn = document.querySelector(
-//   '#mobile-nav .mobile-nav__items #closeBtn'
-// );
-// const dropDownUserMobile = document.querySelector(
-//   '#mobile-nav .mobile-nav__right .dropdown-user .dropbtn'
-// );
-// const dropDownUserMobileContent = document.querySelector(
-//   '#mobile-nav .mobile-nav__right .dropdown-user .dropdown-content'
-// );
+/* ----------------
+1. Side Bar Menu
+2. Backdrop
+3. Close Button
+---------------- */
 
-// /*  ----------  DropDown Menu On Mobile  ----------  */
-// dropDownUserMobile.addEventListener('click', () => {
-//   if (dropDownUserMobileContent.style.display === 'none') {
-//     dropDownUserMobileContent.style.display = 'flex';
-//   } else {
-//     dropDownUserMobileContent.style.display = 'none';
-//   }
-// });
+const backdrop = document.querySelector('.backdrop');
+const togglerMobile = document.querySelector('#mobile-nav #userAction');
+const togglerDesktop = document.querySelector('#main-nav #userAction');
+const userMenu = document.querySelector('#userActionMenu');
+const closeBtn = document.querySelector('#userActionMenu #closeBtn');
 
-// /*  ----------  Mobile Navbar Open and Close  ----------  */
-// closeBtn.addEventListener('click', () => {
-//   backdrop.classList.remove('show');
-//   menu.classList.remove('show');
-// });
-// backdrop.addEventListener('click', () => {
-//   backdrop.classList.remove('show');
-//   menu.classList.remove('show');
-//   searchBar.classList.remove('show');
-// });
-// menuToggler.addEventListener('click', () => {
-//   backdrop.classList.toggle('show');
-//   menu.classList.toggle('show');
-// });
+/*  ----------  Navbar Open and Close  ----------  */
+closeBtn.addEventListener('click', () => {
+  backdrop.classList.add('hide');
+  userMenu.classList.add('hide');
+});
 
-// // Search Bar Open and Close
-// searchBtnMob.addEventListener('click', () => {
-//   backdrop.classList.toggle('show');
-//   searchBar.classList.toggle('show');
-// });
-// searchBtnMain.addEventListener('click', () => {
-//   backdrop.classList.toggle('show');
-//   searchBar.classList.toggle('show');
-// });
+backdrop.addEventListener('click', () => {
+  backdrop.classList.add('hide');
+  userMenu.classList.add('hide');
+});
 
-// // window.addEventListener('scroll', () => {
-// //   if (window.scrollY > 150) {
-// //     document.querySelector('#main-nav').style.opacity = 0.9;
-// //     document.querySelector('#mobile-nav').style.opacity = 0.9;
-// //   } else {
-// //     document.querySelector('#main-nav').style.opacity = 1;
-// //     document.querySelector('#mobile-nav').style.opacity = 1;
-// //   }
-// // });
+togglerMobile.addEventListener('click', () => {
+  backdrop.classList.remove('hide');
+  userMenu.classList.remove('hide');
+});
 
+togglerDesktop.addEventListener('click', () => {
+  backdrop.classList.remove('hide');
+  userMenu.classList.remove('hide');
+});
